7387b2113295b158231e48e6cf134f401579f633
plla2 <120915990+plla2@users.noreply.github.com>
2023-01-22 22:05:29 +0900

    JS 1.1

--- roompage-js/main.js
+++ roompage-js/main.js
@@ -24,6 +24,12 @@ let desktopPics = [
   "url(./images/desktop-image-hero-3.jpg)",
 ];
 
+let hOneTexts = [
+  "Discover innovation ways to decorate",
+  "We are available all across the globe",
+  "Manufactured with the best materials",
+];
+
 let headersTexts = [
   "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
   "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
@@ -50,4 +56,11 @@ function nextSlide() {
       back.style.backgroundImage = mobilePics[counta];
     });
   }
+  if (countb >= hOneTexts.length - 1) return;
+  countb++;
+  headerHOne.forEach((hone) => (hone.innerHTML = hOneTexts[countb]));
+
+  if (countc >= headerTexts.length - 1) return;
+  countc++;
+  headerTexts.forEach((texts) => (texts.innerHTML = headersTexts[countc]));
 }
