fd4af776263066b6dd798616fc258d2415eb9a14
plla2 <jhbljs92@naver.com>
2023-01-27 18:52:23 +0900

    first commit(complete)

---
+++ filter-js/products.js
@@ -0,0 +1,98 @@
+const products = [
+  {
+    id: "",
+    title: "santafe",
+    company: "hyundai",
+    image:
+      "https://cdn.autotribune.co.kr/news/photo/202205/6781_38325_1550.jpg",
+    price: 35684.45,
+  },
+  {
+    id: "",
+    title: "kona",
+    company: "hyundai",
+    image:
+      "https://www.hyundai.co.kr/image/upload/asset_library/MDA00000000000017272/031767a7498349c09e58467f42228071.jpg",
+    price: 22784.58,
+  },
+  {
+    id: "",
+    title: "g80",
+    company: "genesis",
+    image:
+      "https://www.genesis.com/content/dam/genesis-p2/kr/assets/utility/sns/genesis-kr-model-g80-02-social-1200x630-ko.jpg",
+    price: 67315.49,
+  },
+  {
+    id: "",
+    title: "k8",
+    company: "kia",
+    image:
+      "https://www.hyundai.co.kr/image/upload/asset_library/MDA00000000000009104/70f240f97225442c9219c4d75228ebf0.jpg",
+    price: 34077.08,
+  },
+  {
+    id: "ssangyong",
+    title: "tivoli",
+    company: "ssangyong",
+    image:
+      "https://blog.kakaocdn.net/dn/dytxHu/btrDhhW4kzf/MzXoPh6SW4R3GmlTeer6mk/img.png",
+    price: 17038.54,
+  },
+  {
+    id: "genesis",
+    title: "gv80",
+    company: "genesis",
+    image:
+      "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMjEyMjFfNDIg%2FMDAxNjcxNjEzNzM2MTM0.Lo14k16D-D8v9Ed_6UwGATw322njKbioO3Jr6AXa9b8g._h0XS_hjVrFLhWDMVFYjs2hhjK0ZEw8Bjuntu3fgw5kg.JPEG.bright_012%2Fgenesis-gv80-21-128.jpg&type=sc960_832",
+    price: 51926.98,
+  },
+  {
+    id: "kia",
+    title: "mohavi",
+    company: "kia",
+    image:
+      "https://imgauto-phinf.pstatic.net/20220107_184/auto_1641524493801S8XbF_JPEG/20220107120131_wjxkQ89S.jpg",
+    price: 47058.82,
+  },
+  {
+    id: "ssangyong",
+    title: "rexton",
+    company: "ssangyong",
+    image:
+      "https://imgauto-phinf.pstatic.net/20230102_278/auto_1672632427132dK6Sv_JPEG/20230102130702_2MCcxXX1.jpg",
+    price: 34888.44,
+  },
+  {
+    id: "hyundai",
+    title: "nexo",
+    company: "hyundai",
+    image:
+      "https://imgauto-phinf.pstatic.net/20210119_293/auto_1611016555146RGCN5_JPEG/20210119093529_WpfbutWe.jpg",
+    price: 56795.13,
+  },
+  {
+    id: "hyundai",
+    title: "palisade",
+    company: "hyundai",
+    image:
+      "https://imgauto-phinf.pstatic.net/20220518_96/auto_1652838679294bJXhS_JPEG/20220518105044_bJ5q9SQe.jpg",
+    price: 40567.95,
+  },
+  {
+    id: "kia",
+    title: "stinger",
+    company: "kia",
+    image:
+      "https://imgauto-phinf.pstatic.net/20221222_14/auto_1671670686923mMeAS_JPEG/20221222095803_MvyvP9Xp.jpg",
+    price: 38945.23,
+  },
+  {
+    id: "genesis",
+    title: "g90",
+    company: "genesis",
+    image:
+      "https://imgauto-phinf.pstatic.net/20221116_123/auto_1668566307081huFBq_JPEG/20221116113818_YmpbmLDp.jpg",
+    price: 73022.31,
+  },
+];
