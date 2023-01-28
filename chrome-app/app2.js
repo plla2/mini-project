19d9fb9b81c85417bf97157247280941a5b197e5
plla2 <jhbljs92@naver.com>
2023-01-28 19:41:12 +0900

    first commit

---
+++ chrome-app/app2.js
@@ -0,0 +1,7 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+
+loginButton.addEventListener("click", () => {
+  console.log("click!");
+});
