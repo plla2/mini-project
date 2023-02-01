31aec95d8db4669669ecec4e45463bdbcb5ab9ad
plla2 <jhbljs92@naver.com>
2023-02-01 14:19:44 +0900

    third-commit(complete)

--- random-password-js/app.js
+++ random-password-js/app.js
@@ -1,5 +1,8 @@
 const lengthSlider = document.querySelector(".pass-length input"),
   options = document.querySelectorAll(".option input"),
+  passwordInput = document.querySelector(".input-box input"),
+  passIndicator = document.querySelector(".pass-indicator"),
+  copyIcon = document.querySelector(".input-box span"),
   generateBtn = document.querySelector(".generate-btn");
 
 const characters = {
@@ -12,29 +15,62 @@ const characters = {
 const generatePassword = () => {
   let staticPassword = "",
     randomPassword = "",
+    excludeDuplicate = false,
     passLength = lengthSlider.value;
 
   options.forEach((option) => {
     // looping through each option's checkbox
     if (option.checked) {
       // if checkbox is checked
-      staticPassword += characters[option.id]; //adding particular key value from character object to staticPassword
+      if (option.id !== "exc-duplicte" && option.id !== "spaces") {
+        staticPassword += characters[option.id]; //adding particular key value from character object to staticPassword
+      } else if (option.id === "spaces") {
+        staticPassword += `  ${staticPassword}  `;
+      } else {
+        excludeDuplicate = true;
+      }
     }
   });
 
   for (let i = 0; i < passLength; i++) {
-    randomPassword +=
+    let randomChar =
       staticPassword[Math.floor(Math.random() * staticPassword.length)];
+    if (excludeDuplicate === true) {
+      !randomPassword.includes(randomChar) || randomChar === " "
+        ? (randomPassword += randomChar)
+        : i--;
+    } else {
+      randomPassword += randomChar;
+    }
   }
+  passwordInput.value = randomPassword;
+};
 
-  console.log(randomPassword);
+const updatePassIndicator = () => {
+  passIndicator.id =
+    lengthSlider.value <= 8
+      ? "weak"
+      : lengthSlider.value <= 16
+      ? "medium"
+      : "strong";
 };
 
 const updateSlider = () => {
   // passing slider value as counter text
   document.querySelector(".pass-length span").innerText = lengthSlider.value;
+  generatePassword();
+  updatePassIndicator();
 };
 updateSlider();
 
+const copyPassword = () => {
+  navigator.clipboard.writeText(passwordInput.value);
+  copyIcon.innerText = "complete";
+  setTimeout(() => {
+    copyIcon.innerText = "copy_all";
+  }, 1500);
+};
+
+copyIcon.addEventListener("click", copyPassword);
 lengthSlider.addEventListener("input", updateSlider);
 generateBtn.addEventListener("click", generatePassword);
