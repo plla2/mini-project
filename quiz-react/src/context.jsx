91269963c21ade1df57dd11f6bbd2e0aaac66e37
plla2 <jhbljs92@naver.com>
2023-02-15 11:53:12 +0900

    second-commit(edit components)

--- quiz-react/src/context.jsx
+++ quiz-react/src/context.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import axios from "axios";
 
+const table = {
+  sports: 21,
+  history: 23,
+  politics: 24,
+};
+
+const API_ENDPOINT = "https://opentdb.com/api.php?";
+const url = "";
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+const AppContext = React.createContext();
 function context() {
   return <div></div>;
 }
